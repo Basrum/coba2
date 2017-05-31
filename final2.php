43c56a4d0e2393cff36247d469296ffbd13d3724
Basrum <basrum96@gmail.com>
2017-05-31 09:52:27 +0700

    versi 1.0.0

---
+++ final2.php
@@ -0,0 +1,48 @@
+<?php
+ 
+$server = "localhost" ;
+$username = "root" ;
+$password = "" ;
+$database = "60200114037";
+
+ 
+
+mysql_connect($server,$username,$password) or die ("Koneksi database gagal");
+mysql_select_db($database) or die ("Database tidak tersedia");
+ 
+	echo '<h3>Data User</h3>
+		<table>
+		<tr>
+		<th>nik</th>
+		<th>nama</th>
+		<th>usia</th>
+		<th>alamat</th>
+		<th>tempatlahir</th>
+		<th>tgllahir</th>
+		</tr>
+<tr>';
+ 
+$i=0; 
+
+$tampil = "SELECT * FROM kariywan ORDER BY ASC";
+
+$sql = mysql_query($tampil);
+ 
+//tampilkan seluruh data yang ada pada tabel kariywan
+while($data = mysql_fetch_array($sql))
+ {
+ $i++;
+ 
+echo "
+ <td>".$i."</td>
+ <td>".$data[nik]."</td>
+ <td>".$data[nama]."</td>
+ <td>".$data[usia]."</td>
+ <td>".$data[alamat]."</td>
+  <td>".$data[tempatlahir]."</td>
+   <td>".$data[tgllahir]."</td>
+ </tr>";
+ }
+echo '</table>';
+ 
+?>
---
+++ final3.php
@@ -0,0 +1,49 @@
+<?php
+ 
+$server = "localhost" ;
+$username = "root" ;
+$password = "" ;
+$database = "60200114037";
+
+ 
+
+mysql_connect($server,$username,$password) or die ("Koneksi database gagal");
+mysql_select_db($database) or die ("Database tidak tersedia");
+ 
+	echo '<h3>Data User</h3>
+		<table>
+		<tr>
+		<th>nik</th>
+		<th>nama</th>
+		<th>usia</th>
+		<th>alamat</th>
+		<th>tempatlahir</th>
+		<th>tgllahir</th>
+		</tr>
+<tr>';
+ 
+$i=0; 
+
+$tampil = " SELECT * FROM kariyawan WHERE nama=4 AND umur < 60";
+
+$sql = mysql_query($tampil);
+ 
+//tampilkan seluruh data yang ada pada tabel kariyawan
+while($data = mysql_fetch_array($sql))
+ {
+ $i++;
+ 
+echo "
+ <td>".$i."</td>
+ <td>".$data[nik]."</td>
+ <td>".$data[nama]."</td>
+ <td>".$data[usia]."</td>
+ <td>".$data[alamat]."</td>
+  <td>".$data[tempatlahir]."</td>
+   <td>".$data[tgllahir]."</td>
+   <td> <a href=”no2”>final1.php</a></td>
+ </tr>";
+ }
+echo '</table>';
+ 
+?>
